8a2aad514537dc19bf38f85680d5711f100fa347
Maria Fernanda Catani <133170537+mariacatani@users.noreply.github.com>
2023-10-31 21:55:42 -0300

    placar

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import Details from "../screens/Details";
-import Team from "../../Team";
+import Team from "../screens/Team";
 
 function AppRoutes(){
 
